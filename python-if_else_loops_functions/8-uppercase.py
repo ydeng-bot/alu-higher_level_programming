bd8ad351ce836b9a06f98254865cd4c1c68cac13
Yom Deng <y.deng@alustudent.com>
2026-06-20 12:54:16 +0200

    Revert to direct iteration so non-string input raises TypeError

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in "{}".format(str):
-        print("{}".format(chr(ord(c) - 32) if ord(c) >= 97 and ord(c) <= 122 else c), end="")
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
     print()
