a3a1cc1c9e074fdd1180e263b071d3a72cf2fb5a
Yom Deng <y.deng@alustudent.com>
2026-06-20 12:51:04 +0200

    Reduce to exactly 2 print calls per checker requirement

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in "{}".format(str):
-        if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(chr(ord(c) - 32) if ord(c) >= 97 and ord(c) <= 122 else c), end="")
     print()
