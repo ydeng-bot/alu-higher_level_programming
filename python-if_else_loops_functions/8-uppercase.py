c6ecae6aa31d9845f1198e2bf3e99ad4765b6769
Yom Deng <y.deng@alustudent.com>
2026-06-20 12:42:36 +0200

    Handle non-string input by converting before iterating

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
+    for c in "{}".format(str):
         if ord(c) >= 97 and ord(c) <= 122:
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
