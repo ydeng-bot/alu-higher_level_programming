77ff5530fddeae2c11aea93a7fdefc7942dec952
Yom Deng <y.deng@alustudent.com>
2026-07-01 15:30:09 +0200

    Add 5-no_c.py

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines no_c."""
+
+
+def no_c(my_string):
+    """Remove all characters c and C from a string."""
+    new_string = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_string += char
+    return new_string
