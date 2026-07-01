708a94819d1e3a58583b34e1d62f2ae82982be43
Yom Deng <y.deng@alustudent.com>
2026-07-01 15:24:51 +0200

    Fix syntax error in 3- print_reversed_list_integer.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -7,4 +7,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
     for number in my_list[::-1]:
-        print("{:d}".format(number)))
+        print("{:d}".format(number))
