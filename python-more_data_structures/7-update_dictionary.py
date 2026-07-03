f4598ee1d11020b0aaeefa9ecdea049d0f950a7a
Yom Deng <y.deng@alustudent.com>
2026-07-04 01:18:34 +0200

    Add 7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines update_dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add a key/value pair in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
