81aa96c3635a4dff68603482addb889e846d4306
Yom Deng <y.deng@alustudent.com>
2026-09-26 17:04:50 +0200

    Add 5-request_store.js

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (writeError) => {
+      if (writeError) {
+        console.log(writeError);
+      }
+    });
+  }
+});
