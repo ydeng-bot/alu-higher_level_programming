a2cbd60794e78da7bb8a27f57f327cc7d8090051
Yom Deng <y.deng@alustudent.com>
2026-09-26 16:03:07 +0200

    Add 6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  charPrint (c) {
+    const char = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
