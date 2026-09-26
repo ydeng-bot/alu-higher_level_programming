8c1cd044330626cf78378f508de9db6c4d0d0a0a
Yom Deng <y.deng@alustudent.com>
2026-09-26 16:44:45 +0200

    Add 3-starwars_title.js

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const film = JSON.parse(body);
+    console.log(film.title);
+  }
+});
